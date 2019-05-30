79f76b26f9d70f9f06b8a2f1ad66392942a84f39
林泽锋 <282245889@qq.com>
2019-05-30 17:33:50 +0800

    Initial commit

---
+++ src/main/java/com/oauth/security/demo/OauthSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthSecurityDemoApplication.class, args);
+    }
+
+}
